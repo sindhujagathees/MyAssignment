b56b33d8dcbf4eaf6afae24f05a05fed4fa4d966
sindhujagathees <155611392+sindhujagathees@users.noreply.github.com>
2025-07-21 10:55:33 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,15 @@
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		int i=10;
+		if(i>0) {
+			System.out.println("Number is Positive");
+		}
+		else
+		{
+			System.out.println("Number is Negative");
+		}
+	}
+
+}
